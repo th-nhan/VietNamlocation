a2193112dbc94e989d734b3111f5ad91877c22d6
th-nhan <tthhaannnnhhaann@gmail.com>
2025-02-09 16:06:38 +0700

    add js home

---
+++ home.js
@@ -0,0 +1,6 @@
+const signOut = document.querySelector(".all-links li:last-child");
+
+signOut.addEventListener("click", (e)=>{
+    e.preventDefault();
+    window.location.href = "login.html";
+});
