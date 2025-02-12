1c5ec3b11602ebdc09da46313aefb3436c04029a
th-nhan <tthhaannnnhhaann@gmail.com>
2025-02-12 22:20:37 +0700

    fix rv ctn

--- home.js
+++ home.js
@@ -6,3 +6,33 @@ signOut.addEventListener("click", (e)=>{
 });
 
 
+document.addEventListener("DOMContentLoaded", function () {
+  let slides = document.querySelectorAll(".swiper-slider");
+  let currentIndex = 0;
+
+  slides[currentIndex].style.display = "block";
+  slides[currentIndex].classList.add("active");
+
+  
+  function showSlide(index) {
+    slides.forEach(slide => slide.style.display = "none"); // Ẩn tất cả slide
+
+    if (index < 0) {
+        currentIndex = slides.length - 1; // Quay về slide cuối cùng
+    } else if (index >= slides.length) {
+        currentIndex = 0; // Quay về slide đầu tiên
+    } else {
+        currentIndex = index;
+    }
+
+    slides[currentIndex].style.display = "block";
+  }
+  
+  document.getElementById("nextBtn").addEventListener("click", function () {
+      showSlide(currentIndex + 1);
+  });
+
+  document.getElementById("prevBtn").addEventListener("click", function () {
+      showSlide(currentIndex - 1);
+  });
+});
