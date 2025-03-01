bc385e76104f56db77c46ac69ee978e1a67bc076
th-nhan <tthhaannnnhhaann@gmail.com>
2025-03-01 22:10:01 +0700

    rv booking

--- tourservices.js
+++ tourservices.js
@@ -107,3 +107,35 @@ tabs.forEach((tab, index)=>{
 
 
 
+//chay slide
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.swiper-slider');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+document.getElementById('nextBtn').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % totalSlides; 
+    showSlide(currentIndex);
+});
+
+document.getElementById('prevBtn').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides; 
+    showSlide(currentIndex);
+});
+
+
+showSlide(currentIndex);
+
+// services
+document.getElementById('tour-services').addEventListener('click',() => {
+    window.location.href = 'tourservices.html';
+})
